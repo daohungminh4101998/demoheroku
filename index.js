3fdfaa79d664900994858a76faf550428237b5bf
daohungminh <daohungminh4101998@gmail.com>
2021-06-17 02:17:40 +0100

    change file to heroku

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.PORT || 3000
 //Logical OR (||) - JavaScript | MDN
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Hello heroku . i am change file!')
 })
 
 app.listen(port, () => {
